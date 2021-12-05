a80b959b909512a924e84d4d63be3daa5a1cb4da
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-05 08:40:25 +0200

    "create some changes in view.py"

--- project/views.py
+++ project/views.py
@@ -1,20 +1,17 @@
 from django.shortcuts import render
-
-from .models import  Person
+from .models import Person
 import pandas as pd
 
 
 def Import_csv(request):
 
     if request.method == 'POST':
-
         myfile = request.FILES['file']
+        df = pd.read_excel(myfile)
+        for df in df.itertuples():
+            obj = Person.objects.create(first_name=df.first_name, last_name=df.last_name, email=df.email)
+    return render(request, 'input.html')
 
-        empexceldata = pd.read_excel(myfile)
-        dbframe = empexceldata
-        for dbframe in dbframe.itertuples():
-            obj = Person.objects.create(first_name=dbframe.first_name, last_name=dbframe.last_name, email=dbframe.email)
-        return render(request, 'input.html')
 
 
 
