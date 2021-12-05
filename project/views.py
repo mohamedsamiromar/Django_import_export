bcf35fe7b269ca20be807514e7ae51f9def9ca46
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-05 13:41:47 +0200

    Check formatting file

--- project/views.py
+++ project/views.py
@@ -1,29 +1,20 @@
 from django.shortcuts import render
+from tablib import Dataset
+
 from .models import Person
 import pandas as pd
+from django.contrib import messages
 
 
 def Import_csv(request):
-
     if request.method == 'POST':
         myfile = request.FILES['file']
-        df = pd.read_excel(myfile)
-        for df in df.itertuples():
-            obj = Person.objects.create(first_name=df.first_name, last_name=df.last_name, email=df.email)
+        file = str(myfile)
+        if not file.endswith('xlsx'):
+            return render(request, 'error.html',
+                          {'errors': 'the file content is not as expected'})
+        else:
+            df = pd.read_excel(myfile)
+            for df in df.itertuples():
+                obj = Person.objects.create(first_name=df.first_name, last_name=df.last_name, email=df.email)
     return render(request, 'input.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
