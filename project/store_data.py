fb2b52c6dceada982e6395b81f724b4c6da991ae
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-12 09:17:05 +0200

    add some chages

--- project/store_data.py
+++ project/store_data.py
@@ -4,5 +4,5 @@ from .models import Person
 
 
 def save_obj(row):
-    Person.objects.create(first_name=row.first_name, last_name=row.last_name, email=row.email,)
+    Person.objects.create(first_name=row.first_name, last_name=row.last_name, email=row.email, created_at=now)
     return save_obj
