c6ad58f19beadf4079f6866b2cd040efd6a7c7c5
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-12 14:45:02 +0200

    add log file in gitignore

--- project/models.py
+++ project/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField(blank=True, unique=True)
+    email = models.EmailField(blank=True)
     created_by = models.CharField(max_length=30, default='system')
     created_at = models.DateTimeField(auto_now=True)
     updated_by = models.CharField(max_length=30, default='system')
