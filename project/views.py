082c0458e92e1caf3793ca676f16e09609a2a32a
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-14 13:11:54 +0200

    Add new function, The function working to update row and comment's for all each line, explain what he's doing, and add line to return numer of updated row and invalid row

--- project/views.py
+++ project/views.py
@@ -13,48 +13,6 @@ logging.basicConfig(filename='person_log.log', level=logging.DEBUG)
 read data from file(excel, CSV) and create Django object from the data file 
 '''
 
-
-#
-#
-# def Import_file(request):
-#     if request.method == 'POST':
-#         myfile = request.FILES['file']
-#
-#         file = str(myfile)
-#         if file.endswith('xlsx') or file.endswith('xls'):  # check if file end with xlsx
-#             df = pd.read_excel(myfile)
-#         elif file.endswith('csv'):  # check if file end with csv
-#             df = pd.read_csv(myfile)
-#         else:  # if the file does not end with xlsx or csv will return this  message
-#             return render(request, 'messages.html',
-#                           {'messages': 'The File Content Is Not As Expected'})
-#
-#         invalid_row = 0  # the counter will count the invalid row
-#         valid_row = 0  # the counter will count the valid row
-#         updated_row = 0  # the counter will count the updated row
-#
-#         for index, row in df.iterrows():  # The loop reads the rows from the file
-#             if check_row(row, index):  # here will skip any empty row
-#                 invalid_row += 1  # Add 1 to counter
-#             else:
-#                 valid_row += 1  # if file hasn't got any empty row will add 1 to counter
-#                 try:
-#                     get_email = Person.objects.get(email=row.email)
-#                 except Person.DoesNotExist:
-#                     get_email = None
-#                 if get_email is None:
-#                     save_obj(row)
-#                 else:
-#                     update_row(get_email, row)
-#                     updated_row += 1
-#                     logging.debug(('Valid Row : {}'.format(updated_row), 'Invalid Row: {}'.format(invalid_row)))
-#                     return render(request, 'messages.html', {"messages": "Update Successes"})
-#
-#             logging.debug(('Valid Row : {}'.format(valid_row), 'Invalid Row: {}'.format(invalid_row)))
-#             return render(request, 'messages.html', {"messages": "Success"})
-#     return render(request, 'upload_excel_file.html')
-
-
 def Import_file(request):
     if request.method == 'POST':
         myfile = request.FILES['file']
