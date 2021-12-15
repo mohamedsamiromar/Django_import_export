44bf34ae0e8b64bd467e6b535263d63bbc11ae76
mohamed samir <56839321+mohamedsamiromar@users.noreply.github.com>
2021-12-15 16:41:50 +0200

    add function return any empty row in log file

--- project/row.py
+++ project/row.py
@@ -6,8 +6,7 @@ this function will check if the file has gotten an empty row, will skip the row
 '''
 #
 #
-#
-# def check_row(row, index, df):
+# def check_row(row, index):
 #     if pd.isna(row[0]):
 #         empty_row = ('Index: {}'.format(index), 'First name: {}'.format(row.first_name),
 #                      'Last name: {}'.format(row.last_name), 'email: {}'.format(row.email),
@@ -19,9 +18,9 @@ this function will check if the file has gotten an empty row, will skip the row
 
 
 def check_row(row, index):
-    if row.notnull().all():
-        empty_row = ('Index: {}'.format(index), 'SKU: {}'.format(row.sku),
+    if pd.isna(row):
+        info_empty_row = ('Index: {}'.format(index), 'SKU: {}'.format(row.sku),
                      'image_links: {}'.format(row.image_links), 'attachment_links: {}'.format(row.attachment_links))
-        logging.debug(empty_row)
+        logging.debug(info_empty_row)
 
         return check_row
--- project/views.py
+++ project/views.py
@@ -4,6 +4,8 @@ import logging
 from .models import Person, ReadUpdate
 from .store_data import save_obj
 from .update import update_row
+from .row import check_row
+
 logging.basicConfig(filename='person_log.log', level=logging.DEBUG)
 
 '''
@@ -23,23 +25,31 @@ def Import_file(request):
         else:  # if the file does not end with xlsx or csv will return this  message
             return render(request, 'messages.html',
                           {'messages': 'The File Content Is Not As Expected'})
-
         invalid_row = 0  # the counter will count the invalid row
+
+        # return the empty row in log file
+        invalid_row = df.isnull().sum().sum()
+        for index, row in df.iterrows():
+            if df.isna:
+                info_empty_row = ('Index: {}'.format(index), 'SKU: {}'.format(row.sku),
+                                  'image_links: {}'.format(row.image_links),
+                                  'attachment_links: {}'.format(row.attachment_links))
+                logging.debug(info_empty_row)
+
+        df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)  # Here will skip any empty row
+
         valid_row = 0  # the counter will count the valid row
         updated_row = 0  # the counter will count the updated row
-        df.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True)  # Here will drop any empty row
 
         for index, row in df.iterrows():  # The loop reads the rows from the file
-
             # check if the row exists will update the row, if not exist will insert a row in the database
             try:
                 get_row = ReadUpdate.objects.get(sku=row.sku)
             except ReadUpdate.DoesNotExist:
                 get_row = None
             if get_row is None:
-
                 save_obj(row)  # The function working to insert row in database
-
+                valid_row += 1
             else:
                 update_row(get_row, row)  # The function working to update row
                 updated_row += 1
